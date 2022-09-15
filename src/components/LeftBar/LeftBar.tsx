a75de6523d8e24ff2cbf492a1b6e34e832a8b69f
zhunor <ozerorhun@gmail.com>
2022-09-16 01:50:17 +0300

    upgrade deps

--- src/components/LeftBar/LeftBar.tsx
+++ src/components/LeftBar/LeftBar.tsx
@@ -1,9 +1,10 @@
-import { Tabs } from 'antd';
+import { Button, Modal, Tabs } from 'antd';
 import React from 'react';
 import { FiDatabase, FiMap } from 'react-icons/fi';
 
 import styles from './style.module.scss';
 export const LeftBar = () => {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
   const tabs = [
     {
       label: (
@@ -25,9 +26,32 @@ export const LeftBar = () => {
     },
   ];
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className={styles.LeftBar}>
       <Tabs defaultActiveKey="2" items={tabs} />
+      <div className={styles.LeftBarFooter}>
+        <Button type="primary" block onClick={openModal} className={styles.ImportButton}>
+          Add Layer{' '}
+        </Button>
+        <Modal
+          title="Basic Modal"
+          open={isModalOpen}
+          onOk={closeModal}
+          onCancel={closeModal}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
     </div>
   );
 };
