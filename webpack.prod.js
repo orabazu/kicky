bf487f97a1dc9ff87cdbc27009beb71a207f600a
zhunor <ozerorhun@gmail.com>
2022-12-04 01:46:49 +0300

    Add bundle analyzer commented out

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = merge(common, {
   mode: 'production',
@@ -10,5 +12,6 @@ module.exports = merge(common, {
       // exclude: ["favicon.ico"],
       verbose: true
     }),
+    // new BundleAnalyzerPlugin()
   ]
 });
